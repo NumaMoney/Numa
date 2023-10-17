8273f2f3a7034ebfcfcd671fa8caaf5cf746bf86
tib <thibof@hotmail.com>
2023-10-17 21:22:38 +0200

    added hardhat verify and hardhat ledger

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,8 @@
 require("@openzeppelin/hardhat-upgrades");
 require("@nomicfoundation/hardhat-chai-matchers")
 require("@onmychain/hardhat-uniswap-v2-deploy-plugin");
+require("@nomicfoundation/hardhat-ledger");
+require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
 
 
@@ -12,11 +14,26 @@ module.exports = {
   networks: {
     sepolia: {
       url: process.env.URL,
-      accounts: [process.env.PKEY],
+      accounts: [process.env.PKEY2],
     },
     goerli: {
       url: process.env.URL2,
+      accounts: [process.env.PKEY2],
+    },
+    arbitest: {
+      url: process.env.URL4,
       accounts: [process.env.PKEY],
     },
+    arbitrum: {
+      url: process.env.URL3,
+      ledgerAccounts: [
+        "0x47786AC88f8CDB8D3A5FE956e51F95b4aE9636fa",
+      ],
+    },
   },
+  etherscan: {
+    apiKey: {
+        arbitrumOne: "7QJ5HEMUC13QIS29HDZGVMXYSW17EF56VK",
+    }
+  }
 };
