2e49c79910ead07c4c1f318777f5bcf96cfd04bb
tib <thibof@hotmail.com>
2024-01-17 15:45:46 +0100

    removing flexfee and nuAsset/ETH pool as oracle

--- contracts/NumaProtocol/NumaOracle.sol
+++ contracts/NumaProtocol/NumaOracle.sol
@@ -16,18 +16,18 @@ contract NumaOracle is Ownable
     address public immutable weth9;
     uint32 public intervalShort;
     uint32 public intervalLong;
-    uint256 public flexFeeThreshold;
+    
 
     event IntervalShort(uint32 _intervalShort);
     event IntervalLong(uint32 _intervalLong);
     event FlexFeeThreshold(uint256 _flexFeeThreshold);
 
-    constructor(address _weth9,uint32 _intervalShort,uint32 _intervalLong,uint256 _flexFeeThreshold,address initialOwner) Ownable(initialOwner) 
+    constructor(address _weth9,uint32 _intervalShort,uint32 _intervalLong,address initialOwner) Ownable(initialOwner) 
     {
         weth9 = _weth9;
         intervalShort = _intervalShort;
         intervalLong = _intervalLong;
-        flexFeeThreshold = _flexFeeThreshold;
+      
     }
 
 
@@ -45,12 +45,7 @@ contract NumaOracle is Ownable
         emit IntervalLong(intervalLong);
     }
 
-    function setFlexFeeThreshold(uint256 _flexFeeThreshold) external onlyOwner 
-    {
-        require(_flexFeeThreshold <= 1e18, "Flex fee threshold too high");
-        flexFeeThreshold = _flexFeeThreshold;
-        emit FlexFeeThreshold(flexFeeThreshold);
-    }
+
 
     /**
      * @dev Get chainlink price from price feed address
@@ -175,41 +170,7 @@ contract NumaOracle is Ownable
     }
 
 
-    /**
-     * @dev Check if token price using uniswap V3 pool is below threshold 
-     * @notice uses lowest price of 2 intervals inputs (getV3SqrtLowestPrice)
-     * @param {uint256} _threshold 
-     * @param {address} _pool the pool 
-     * @param {uint32} _intervalShort the short interval
-     * @param {uint32} _intervalLong the long interval
-     * @return {bool} true if it's below threshold
-     */
-    function isTokenBelowThreshold(uint256 _threshold, address _pool, uint32 _intervalShort, uint32 _intervalLong, address _chainlinkFeed, address _weth9) public view returns (bool) 
-    {
-        uint160 sqrtPriceX96 = getV3SqrtLowestPrice(_pool, _intervalShort, _intervalLong);
-        //We want _weth9 as our price feed's numerator for the multiplications below
-        uint256 numerator = (IUniswapV3Pool(_pool).token1() == _weth9 ? sqrtPriceX96 : FixedPoint96.Q96);
-        uint256 denominator = (numerator == sqrtPriceX96 ? FixedPoint96.Q96 : sqrtPriceX96);
-        //ETH per Token, times 1e18
-        uint256 ethPerToken = FullMath.mulDiv(FullMath.mulDiv(numerator, numerator, denominator), 1e18, denominator);
 
-
-        if (_chainlinkFeed == address(0)) return ethPerToken < _threshold;
-        uint256 linkFeed = chainlinkPrice(_chainlinkFeed);
-        uint256 decimalPrecision = AggregatorV3Interface(_chainlinkFeed).decimals();
-        uint256 tokensForAmount;
-
-
-
-        //if ETH is on the left side of the fraction in the price feed
-        if (ethLeftSide(_chainlinkFeed)) {
-            tokensForAmount = FullMath.mulDiv(ethPerToken, linkFeed, 10**decimalPrecision);
-        } else {
-            tokensForAmount = FullMath.mulDiv(ethPerToken, 10**decimalPrecision, linkFeed);
-        }
-
-        return tokensForAmount < _threshold;
-    }
 
     /**
      * @dev number of numa tokens needed to mint amount 
@@ -250,54 +211,7 @@ contract NumaOracle is Ownable
         
 
 
-    /**
-     * @dev number of Numa that will be minted by burning this amount of nuAsset using pools only
-     * @param {address} _numaPool Numa pool address
-     * @param {address} _tokenPool nuAsset pool address
-     * @param {uint32} _intervalShort the short interval
-     * @param {uint32} _intervalLong the long interval
-     * @param {uint256} _amount amount we want to burn
-     * @param {address} _weth9 weth address
-     */
-    function getNbOfNumaFromAssetUsingPools(address _numaPool, address _tokenPool, uint32 _intervalShort, uint32 _intervalLong, uint256 _amount, address _weth9) public view returns (uint256) 
-    {
-        // highest price for numa to minimize amount to mint
-        uint160 numaSqrtPriceX96 = getV3SqrtHighestPrice(_numaPool, _intervalShort, _intervalLong);
-        // lowest price for nuAsset to maximize amount to burn
-        uint160 tokenSqrtPriceX96 = getV3SqrtLowestPrice(_tokenPool, _intervalShort, _intervalLong);
-        uint256 numaA;
-        uint256 numaPrice;
-        uint256 tokenA;
-        uint256 tokenPrice;
-
-        // numaPrice is numa/ETH
-        if (IUniswapV3Pool(_numaPool).token0() == _weth9) 
-        {
-            numaA = FullMath.mulDiv(numaSqrtPriceX96, numaSqrtPriceX96, FixedPoint96.Q96);
-            numaPrice = FullMath.mulDiv(numaA, 1e18, FixedPoint96.Q96);
-        }
-        else 
-        {
-            numaA = FullMath.mulDiv(FixedPoint96.Q96, FixedPoint96.Q96, numaSqrtPriceX96);
-            numaPrice = FullMath.mulDiv(numaA, 1e18, numaSqrtPriceX96);
-        }
-
-        // tokenPrice is ETH/Token
-        if (IUniswapV3Pool(_tokenPool).token1() == _weth9) 
-        {
-            tokenA = FullMath.mulDiv(tokenSqrtPriceX96, tokenSqrtPriceX96, FixedPoint96.Q96);
-            tokenPrice = FullMath.mulDiv(tokenA, _amount, FixedPoint96.Q96);
-        }
-        else
-        {
-            tokenA = FullMath.mulDiv(FixedPoint96.Q96, FixedPoint96.Q96, tokenSqrtPriceX96);
-            tokenPrice = FullMath.mulDiv(tokenA, _amount, tokenSqrtPriceX96);
-        }
-
-        //Multiplying numaPrice by tokenPrice and dividing by 1e18
-        //In other words, numa * amount / Tokens -> Number of numa to mint for a given amount
-        return FullMath.mulDiv(numaPrice, tokenPrice, 1e18); 
-    }
+   
 
     /**
      * @dev number of Numa that will be minted by burning this amount of nuAsset using numa pool and chainlink
@@ -351,19 +265,11 @@ contract NumaOracle is Ownable
      * @param {address} _tokenPool nuAsset pool address
      * @return {uint256} amount of Numa that will be minted
      */
-    function getNbOfNumaFromAsset(uint256 _amount, address _chainlinkFeed, address _numaPool, address _tokenPool) external view returns (uint256) 
+    function getNbOfNumaFromAsset(uint256 _amount, address _chainlinkFeed, address _numaPool) external view returns (uint256) 
     {
         uint256 _output;
-        // if nuAsset price using uniswap pool is below threshold we use pool value as price reference
-        if (isTokenBelowThreshold(flexFeeThreshold, _tokenPool, intervalShort, intervalLong, _chainlinkFeed, weth9)) 
-        {
-            _output = getNbOfNumaFromAssetUsingPools(_numaPool, _tokenPool, intervalShort, intervalLong, _amount, weth9);
-        } 
-        else 
-        {
-            // if not we use chainlink price
-            _output = getNbOfNumaFromAssetUsingOracle(_numaPool, intervalShort, intervalLong, _chainlinkFeed, _amount, weth9);
-        }
+        // we use chainlink price
+        _output = getNbOfNumaFromAssetUsingOracle(_numaPool, intervalShort, intervalLong, _chainlinkFeed, _amount, weth9);
         return _output;
     }
 
